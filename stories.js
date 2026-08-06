3174889093252a251d43628ed606980c90fef890
anca <anca.grosan@gmail.com>
2026-08-06 14:03:22 +0300

    image style fix

--- stories.js
+++ stories.js
@@ -6,7 +6,7 @@ STORIES.forEach((story) => {
   const card = document.createElement("div");
   card.className = "story-card";
   card.innerHTML = `
-    ${story.img ? `<img src="${story.img}" alt="${story.title}" />` : ""}
+    ${story.img ? `<img src="${story.img}" class="story-img" alt="${story.title}" />` : ""}
     ${story.title ? `<h3>${story.title}</h3>` : ""}
     <p class="story-author">de ${story.author}</p>
     <p class="story-text">${story.text}</p>
